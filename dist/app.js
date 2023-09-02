c6d7dd46aa6e7e09b873d74ecacf1536b17ecc46
IJproject <junchan3340@gmail.com>
2023-09-02 18:15:21 +0900

    パスワード作成以外は完成

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,10 @@ class Generate {
         this.content = document.getElementById('content');
         this.long = document.getElementById('length');
         this.kind = document.getElementById('kind');
+        this.processContent = [];
+        this.processLong = 0;
+        this.processKind = '';
+        this.password = document.getElementById('password');
         this.generate = document.getElementById('generate');
         this.regenerate = document.getElementById('re-generate');
         this.copy = document.getElementById('copy');
@@ -14,16 +18,38 @@ class Generate {
     Generate() {
         this.generate.addEventListener('click', (event) => {
             event.preventDefault();
+            this.ProcessInputValue();
+            this.MakePassword();
         });
+    }
+    ProcessInputValue() {
+        this.processContent = this.content.value.split(/[\s,]+/);
+        this.processContent = this.processContent.filter(function (item) {
+            return item.trim() !== '';
+        });
+        this.processLong = +this.long.value;
+        this.processKind = this.kind.value;
+    }
+    MakePassword() {
+        console.log('make password');
     }
     ReGenerate() {
         this.regenerate.addEventListener('click', (event) => {
             event.preventDefault();
+            this.MakePassword();
         });
     }
     Copy() {
         this.copy.addEventListener('click', (event) => {
             event.preventDefault();
+            const text = this.password.innerText;
+            const hiddenTextArea = document.createElement("textarea");
+            hiddenTextArea.value = text;
+            this.password.appendChild(hiddenTextArea);
+            hiddenTextArea.select();
+            document.execCommand('copy');
+            this.password.removeChild(hiddenTextArea);
+            alert("コピーしました");
         });
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,30 @@ class Generate {
     long: HTMLInputElement
     kind: HTMLSelectElement
 
+    processContent: string[]
+    processLong: number
+    processKind: string
+
+    password: HTMLParagraphElement
+
     generate: HTMLButtonElement
     regenerate: HTMLButtonElement
     copy: HTMLButtonElement
+
 
     constructor() {
         // 入力要素
         this.content = document.getElementById('content') as HTMLInputElement;
         this.long = document.getElementById('length') as HTMLInputElement;
         this.kind = document.getElementById('kind') as HTMLSelectElement;
+
+        // 加工済みデータ
+        this.processContent = []; 
+        this.processLong = 0;
+        this.processKind = '';
+
+        // 完成したパスワード
+        this.password = document.getElementById('password') as HTMLParagraphElement;
 
         // ボタン
         this.generate = document.getElementById('generate') as HTMLButtonElement;
@@ -25,31 +40,67 @@ class Generate {
         this.Copy();
     }
 
+
     // 作成ボタンの挙動
     private Generate() {
         this.generate.addEventListener('click', (event: Event) => {
             event.preventDefault();
-            
+            this.ProcessInputValue();
+            this.MakePassword();
         })
     }
+
+
+    // 入ってきたデータの加工とバリデーション
+    private ProcessInputValue() {
+        // 含めたい文字の情報をコンマごとに区切り、配列に代入
+        this.processContent = this.content.value.split(/[\s,]+/)
+        this.processContent = this.processContent.filter(function(item) {
+            return item.trim() !== '';
+        });
+
+        this.processLong = +this.long.value
+        this.processKind = this.kind.value
+    }
+
+
+    private MakePassword() {
+        console.log('make password')
+    }
+
 
     // 再作成ボタンの挙動
     private ReGenerate() {
         this.regenerate.addEventListener('click', (event: Event) => {
             event.preventDefault();
+            this.MakePassword();
         })
     }
+
 
     // コピーボタンの挙動
     private Copy() {
         this.copy.addEventListener('click', (event: Event) => {
             event.preventDefault();
+            
+            const text = this.password.innerText;
+
+            const hiddenTextArea = document.createElement("textarea");
+            hiddenTextArea.value = text;
+
+            /* 隠しテキストエリアをDOMに追加 */
+            this.password.appendChild(hiddenTextArea);
+            /* テキストエリア内のテキストを選択 */
+            hiddenTextArea.select();
+            /* コピー操作を実行 */
+            document.execCommand('copy');
+            /* 隠しテキストエリアを削除 */
+            this.password.removeChild(hiddenTextArea);
+
+            alert("コピーしました");
         })
     }
 }
 
-const generate = new Generate();
 
-// setInterval(() => {
-//     console.log(getInput.content.value)
-// }, 1000)
+const generate = new Generate();

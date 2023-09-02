be4139ab216f0ccf845c996c6f42d4ac05eb6a64
IJproject <junchan3340@gmail.com>
2023-09-02 16:48:21 +0900

    make-base-class

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log('init');
+class Generate {
+    constructor() {
+        this.content = document.getElementById('content');
+        this.long = document.getElementById('length');
+        this.kind = document.getElementById('kind');
+        this.generate = document.getElementById('generate');
+        this.regenerate = document.getElementById('re-generate');
+        this.copy = document.getElementById('copy');
+        this.Generate();
+        this.ReGenerate();
+        this.Copy();
+    }
+    Generate() {
+        this.generate.addEventListener('click', (event) => {
+            event.preventDefault();
+        });
+    }
+    ReGenerate() {
+        this.regenerate.addEventListener('click', (event) => {
+            event.preventDefault();
+        });
+    }
+    Copy() {
+        this.copy.addEventListener('click', (event) => {
+            event.preventDefault();
+        });
+    }
+}
+const generate = new Generate();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,55 @@
-console.log('init')
+class Generate {
+
+    content: HTMLInputElement
+    long: HTMLInputElement
+    kind: HTMLSelectElement
+
+    generate: HTMLButtonElement
+    regenerate: HTMLButtonElement
+    copy: HTMLButtonElement
+
+    constructor() {
+        // 入力要素
+        this.content = document.getElementById('content') as HTMLInputElement;
+        this.long = document.getElementById('length') as HTMLInputElement;
+        this.kind = document.getElementById('kind') as HTMLSelectElement;
+
+        // ボタン
+        this.generate = document.getElementById('generate') as HTMLButtonElement;
+        this.regenerate = document.getElementById('re-generate') as HTMLButtonElement;
+        this.copy = document.getElementById('copy') as HTMLButtonElement;
+
+        // 各ボタンのイベントを追加
+        this.Generate();
+        this.ReGenerate();
+        this.Copy();
+    }
+
+    // 作成ボタンの挙動
+    private Generate() {
+        this.generate.addEventListener('click', (event: Event) => {
+            event.preventDefault();
+            
+        })
+    }
+
+    // 再作成ボタンの挙動
+    private ReGenerate() {
+        this.regenerate.addEventListener('click', (event: Event) => {
+            event.preventDefault();
+        })
+    }
+
+    // コピーボタンの挙動
+    private Copy() {
+        this.copy.addEventListener('click', (event: Event) => {
+            event.preventDefault();
+        })
+    }
+}
+
+const generate = new Generate();
+
+// setInterval(() => {
+//     console.log(getInput.content.value)
+// }, 1000)
